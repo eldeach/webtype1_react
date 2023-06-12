7841d19c21e0c3e013c133f839fefcae1dd88e58
Donhyeong Lee <dhm_w@icloud.com>
2023-06-12 23:27:49 +0900

    jwt 시도중

--- src/WebServiceFrame/Login/Form/Functions/onSubmitFunc.js
+++ src/WebServiceFrame/Login/Form/Functions/onSubmitFunc.js
@@ -3,7 +3,36 @@
 import axios from 'axios';
 
 async function onSubmitFunc(props){
-    await axios.post('/local-login',props)
+//     const generateJwtToken = (payload, secret) => {
+//         const token = jwt.sign(payload, secret);
+//         return token;
+//       };
+//       // 사용 예시
+// const payload = { user_account: props.user_account, user_pw: props.user_pw};
+// const secret = 'secret';
+// const jwtToken = generateJwtToken(payload, secret);
+// console.log(jwtToken);
+
+//     await axios.post('/local-login',props, {
+//         headers: {
+//           Authorization: `Bearer ${jwtToken}`,
+//         },
+//       })
+
+
+await axios.post('/local-login' ,{
+    username:"a",
+    password:'bb'
+
+})
 }
 
 export default onSubmitFunc
+
+
+
+
+
+
+
+
